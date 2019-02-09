f1aba05019f16dcaecb2157c8aa3202e7de6148b
Jacob Petrisko <petrisjj@petrisjj-5520.rose-hulman.edu>
2019-02-09 15:28:12 -0500

    fixed password

--- Login.php
+++ Login.php
@@ -35,6 +35,16 @@ session_start();
       transition: 0.5s;
       outline: none;
     }
+input[type=password] {
+      width: 100%;
+      padding: 12px 20px;
+      margin: 8px 0;
+      box-sizing: border-box;
+      border: 3px solid #ccc;
+      -webkit-transition: 0.5s;
+      transition: 0.5s;
+      outline: none;
+    }
 
     input[type=text]:focus {
       border: 3px solid #555;
