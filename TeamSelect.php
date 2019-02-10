fc2be155bab3e5d5932775b4decb53f05876bb2f
Jacob Petrisko <petrisjj@petrisjj-5520.rose-hulman.edu>
2019-02-09 21:13:28 -0500

    asd

--- TeamSelect.php
+++ TeamSelect.php
@@ -32,7 +32,7 @@ session_start();
 
 	while ($row = $result->fetch_array()) {
 
-		echo '<center><font size="64" color ="black">' . $row['firstName'] . ' ' . $row['lastName'] .'</font></center>';
+		echo '<center><color ="black">' . $row['firstName'] . ' ' . $row['lastName'] .'</font></center>';
 		echo '</br>';
 	}
 
