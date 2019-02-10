69f013a65d7d7e2e7febd96f4dd9979e8404d6c9
Jacob Petrisko <petrisjj@petrisjj-5520.rose-hulman.edu>
2019-02-09 20:05:11 -0500

    asd

--- TeamView.php
+++ TeamView.php
@@ -74,9 +74,11 @@ $stmt->bind_param("s", $_GET['TeamName']);
       			{
           	
             echo '<span style = "font-size: 150%">';
-						echo '<a href = TeamView.php?TeamName=', urlencode( $row['Team1']), '> ' . $row['Team1'] . ' </a>';
+						//echo '<a href = TeamView.php?TeamName=', urlencode( $row['Team1']), '> ' . $row['Team1'] . ' </a>';
+            echo $row['Team1'];
             echo ' VS ';
-            echo '<a href = TeamView.php?TeamName=', urlencode( $row['Team2']), '> ' . $row['Team2'] . ' </a>';
+            echo $row['Team2'];
+            //echo '<a href = TeamView.php?TeamName=', urlencode( $row['Team2']), '> ' . $row['Team2'] . ' </a>';
             echo ' AT ' . $row['StartTime'] . '  ' . $row['Location'] . ' | Score: ' . $row['Team1Score'] . ' - ' . $row['Team2Score'];
             echo '</span>';
 						echo '</br>';
