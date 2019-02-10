2796b2d94690db1acc57b0477c6e11efab956b39
Jacob Petrisko <petrisjj@petrisjj-5520.rose-hulman.edu>
2019-02-09 20:08:04 -0500

    fized

--- TeamView.php
+++ TeamView.php
@@ -74,17 +74,17 @@ $stmt->bind_param("s", $_GET['TeamName']);
       			{
           	
             echo '<span style = "font-size: 150%">';
-						//echo '<a href = TeamView.php?TeamName=', urlencode( $row['Team1']), '> ' . $row['Team1'] . ' </a>';
-            echo $row['Team1'];
+						echo '<a href = TeamView.php?TeamName=', urlencode( $row['Team1']), '> ' . $row['Team1'] . ' </a>';
+           // echo $row['Team1'];
             echo ' VS ';
-            echo $row['Team2'];
-            //echo '<a href = TeamView.php?TeamName=', urlencode( $row['Team2']), '> ' . $row['Team2'] . ' </a>';
+          //  echo $row['Team2'];
+            echo '<a href = TeamView.php?TeamName=', urlencode( $row['Team2']), '> ' . $row['Team2'] . ' </a>';
             echo ' AT ' . $row['StartTime'] . '  ' . $row['Location'] . ' | Score: ' . $row['Team1Score'] . ' - ' . $row['Team2Score'];
             echo '</span>';
 						echo '</br>';
 
       			}
-			<a href = TeamView.php?TeamName=', urlencode( $row['Team1']), '> ' . $row['Team1'] . ' </a>
+			
 
   			$stmt->close();
 
