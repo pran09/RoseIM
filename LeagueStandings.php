b804611574e23df085f44316775c79b9bb4a62c0
Jacob Petrisko <petrisjj@petrisjj-5520.rose-hulman.edu>
2019-02-13 12:06:14 -0500

    added the file

---
+++ LeagueStandings.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['emailAddress'])) {
+    header('location: Login.php');
+    exit(); // <-- terminates the current script
+  }
+
+?>
+<head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>League Standings</title>
+      <link rel="stylesheet" href="normalize.css">
+      <link href='https://fonts.googleapis.com/css?family=Nunito:400,300' rel='stylesheet' type='text/css'>
+      <link rel="stylesheet" href="css/main.css">
+      <title>League Standings</title>
+  </head>
+   <center><font size="200" color ="black">Rose</font><font size ="128" color="red">IM</font></center>
+  <div class = "container" style = "background-color:#f4f7f8">
+   </br>
+   
+  	<?php
+	
+
+
+?> 
+
+</div>
+
+
+  <body>
+	
+
+  </body>
+  
+  
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
