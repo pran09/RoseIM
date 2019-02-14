c95e0d205dda188345a83b69e52a1ca77665f4ed
Jacob Petrisko <petrisjj@petrisjj-5520.rose-hulman.edu>
2019-02-14 00:45:23 -0500

    asd

--- TeamSelect.php
+++ TeamSelect.php
@@ -36,6 +36,59 @@ if (!isset($_SESSION['emailAddress'])) {
   			box-shadow: 0 -1px 0 rgba(100,100,100,0.1) inset;
   			margin-bottom: 7px;
 		}
+
+		/* Popup container */
+.popup {
+  position: relative;
+  display: inline-block;
+  cursor: pointer;
+}
+
+/* The actual popup (appears on top) */
+.popup .popuptext {
+  visibility: hidden;
+  width: 160px;
+  background-color: #555;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 8px 0;
+  position: absolute;
+  z-index: 1;
+  bottom: 125%;
+  left: 50%;
+  margin-left: -80px;
+}
+
+/* Popup arrow */
+.popup .popuptext::after {
+  content: "";
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: #555 transparent transparent transparent;
+}
+
+/* Toggle this class when clicking on the popup container (hide and show the popup) */
+.popup .show {
+  visibility: visible;
+  -webkit-animation: fadeIn 1s;
+  animation: fadeIn 1s
+}
+
+/* Add animation (fade in the popup) */
+@-webkit-keyframes fadeIn {
+  from {opacity: 0;} 
+  to {opacity: 1;}
+}
+
+@keyframes fadeIn {
+  from {opacity: 0;}
+  to {opacity:1 ;}
+}
 	</style>
 
 </head>
@@ -109,9 +162,8 @@ function myFunction() {
 	while ($row = $result->fetch_array()) {
 
 		echo '<span style = "font-size: 150%">Team <a href = TeamView.php?TeamName=', urlencode( $row['team_ID']), '> ' . $row['Team'] . ' </a> | ';
-		echo '<a href = LeagueStandings.php?LeagueID=', urlencode( $row['league_ID']), '> ' . $row['League'] . ' </a> ' . '<div class="popup" onclick="myFunction()">' . $row['Sport'].
-  		'<span class="popuptext" id="myPopup">' . $row['Rules'] . '</span>
-		</div>' .  '  </span>';
+		echo '<a href = LeagueStandings.php?LeagueID=', urlencode( $row['league_ID']), '> ' . $row['League'] . ' </a> </span>';
+		echo '<div class="popup" onclick="myFunction()">' . $row['Sport']. '<span class="popuptext" id="myPopup">' . $row['Rules'] . '</span> </div>';
 		echo '</br>';
 
 		
