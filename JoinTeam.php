36b7d94035efc661d90eb312d7c173288ee97879
Praneet Chakraborty <p.chak@outlook.com>
2019-02-14 12:48:38 -0500

    join team

--- JoinTeam.php
+++ JoinTeam.php
@@ -82,7 +82,7 @@
       			$stmt->execute();
       			$result = $stmt->get_result();
 				echo '<label>Sport:</label>';
-				echo '<select name="Sport" id="sport" onChange="showLeague(this.value)">';
+				echo '<select name="Sport" id="sport" onchange="showLeague(this.value)">';
 				$aResult = 5;
       			while ($row = $result->fetch_array(MYSQLI_NUM)) {
           			foreach ($row as $r) {
@@ -134,4 +134,3 @@
     </form>
   </body>
 </html>
-
