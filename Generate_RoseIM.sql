0c38dc9992f7b63970043fa6b708c61695cae626
Praneet Chakraborty <p.chak@outlook.com>
2019-02-15 12:29:19 -0500

    generate db done

--- Generate_RoseIM.sql
+++ Generate_RoseIM.sql
@@ -947,3 +947,247 @@ Begin
 	RETURN 0;
 
 END//
+
+CREATE PROCEDURE `Add_Sport_Data`()
+BEGIN
+
+	SET @a = Create_Sport('Softball', 'Hit the ball and run through the bases.');
+    SET @b = Create_Sport('Basketball', 'Shoot the ball into the hoop.');
+    SET @c = Create_Sport('Flag Football', 'Run or catch the ball in the endzone.');
+    SET @d = Create_Sport('Ultimate Frisbee', 'Catch the frisbee in the endzone.');
+    SET @e = Create_Sport('Volleyball', 'Hit the ball onto the other side of the net. 3 hits allowed per side. Ball cannot touch the ground.');
+	SET @x = Create_Sport('Soccer', 'Kick the ball into the goal. No hands allowed.');
+
+END//
+
+CREATE PROCEDURE `Add_League_Data`()
+BEGIN
+
+	Set @a = Create_League('A League', 'Softball');
+    SET @b = Create_League('B League', 'Softball');
+    SET @c = Create_League('C League', 'Softball');
+    SET @d = Create_League('Greek League', 'Softball');
+
+    SET @a = Create_League('A League', 'Basketball');
+    SET @b = Create_League('B League', 'Basketball');
+    SET @c = Create_League('C League', 'Basketball');
+    SET @d = Create_League('Greek League', 'Basketball');
+    
+    SET @a = Create_League('A League', 'Flag Football');
+    SET @b = Create_League('B League', 'Flag Football');
+    SET @c = Create_League('C League', 'Flag Football');
+    SET @d = Create_League('Greek League', 'Flag Football');
+    
+    SET @a = Create_League('A League', 'Ultimate Frisbee');
+    SET @b = Create_League('B League', 'Ultimate Frisbee');
+    SET @c = Create_League('C League', 'Ultimate Frisbee');
+    SET @d = Create_League('Greek League', 'Ultimate Frisbee');
+    
+    SET @a = Create_League('A League', 'Volleyball');
+    SET @b = Create_League('B League', 'Volleyball');
+    SET @c = Create_League('C League', 'Volleyball');
+    SET @d = Create_League('Greek League', 'Volleyball');
+    
+	SET @a = Create_League('A League', 'Soccer');
+    SET @b = Create_League('B League', 'Soccer');
+    SET @c = Create_League('C League', 'Soccer');
+    SET @d = Create_League('Greek League', 'Soccer');
+
+END//
+
+CREATE PROCEDURE `Add_Person_Data`()
+BEGIN
+
+	SET @a = Create_Person('Joe', 'Hood', 'hood.joe@rose-hulman.edu', '$2y$10$GVIUE.Eq/r/7MQbrWoXWkObCnnHdn308Rm6YYbT62b7SaJW4bM19i','Male');	#hello
+    SET @b = Create_Person('Johnson', 'Joe', 'johnson.joe@rose-hulman.edu', '$2y$10$iRtwZvI/d4nnxNnFYbjHMOMFtCcvYL2tORwTxoejRX8PBBJHEVNDS','Male');	# personsyeet
+    SET @c = Create_Person('Jacob', 'Petrisko', 'petrisjj@rose-hulman.edu', '$2y$10$QuAP9R8B4jNF76GJ9h/d1uTyi5gFtaqa9wpzlfgdeDi8Sq09frimK','Male');	# blue
+    SET @d = Create_Person('Jose', 'Alvarez', 'alvarez@rose-hulman.edu', '$2y$10$vo1h5gKpBCT36Yb5ijxfheP0v6IFDfalT8lL2ikBUcNbBdXvqJN4K','Male');	# yellow
+    SET @e = Create_Person('Bob', 'Sutton', 'sutton@rose-hulman.edu', '$2y$10$UyLyWsrvxLD1f0bFkk11V.O2TNMmULQqYlyTmZD8kr1uJ7fDbawqO','Male');	# somethingElseYuh128475
+	SET @f = Create_Person('James', 'Taylor', 'tayjames@rose-hulman.edu', '$2y$10$E9BIvKh2YTs9Dsg3lk9iXeM4KdJes9NWBPqrYh0uCTW.YOPLupHaK','Male');	# routine
+    SET @g = Create_Person('Jack', 'Ryan', 'ryan.jack@rose-hulman.edu', '$2y$10$.7pnqp79l3.rF9cXq0wPWuKAqLEGT.tg4HwP6NXBfbPbLfCHR9Bly','Male');		# data
+    SET @h = Create_Person('Praneet', 'Chakraborty', 'chakrap@rose-hulman.edu', '$2y$10$3QN/lJnV/Q8R23tJU6bPQOYJo3.VLfJRcSRtZwp4S4gumDXiiaf2u','Male');	# mysqlYuhY7
+	SET @i = Create_Person('Willy', 'Smith', 'bruh@rose-hulman.edu', '$2y$10$W7.IyW8kpauyPZJXiz4rTOcIhnR0Q11yZAwllUABK/dE5grl/YtSC','Male');	# sriram_mohan
+    SET @j = Create_Person('Ryan', 'Dinkleburg', 'dinklebuuuuuurg@rose-hulman.edu', '$2y$10$6ebkEc9Xn5sRUbbAGn1v1.2td7LQSCj9QjysSOPaYP5dFxwhBkjiu','Male');	# databases333
+    SET @k = Create_Person('Sam', 'Flickinger', 'flick@rose-hulman.edu', '$2y$10$DP2iAD7KkVnatm28q.n.1eBv6yrXvYuJxAftGJ1YxU5OALpo4KhOu','Male');	# 333-03
+    SET @l = Create_Person('Chang', 'Wang', 'wutangclan@rose-hulman.edu', '$2y$10$hziwBmSHc7WxIiqM9s8JT.CQTULlZWvJfWBAScxoP.fRThM3SnzU6','Male');	# apply!revert
+    SET @m = Create_Person('Reeve', 'Vixon', 'vixon@rose-hulman.edu', '$2y$10$K3B4eIZIjBs2dOiqJsPAee9MN3l/g3FlsJrKmxIs0pWkE68qwhnJ6','Male');	# generate_tables
+    SET @n = Create_Person('Alexander', 'Pikachu', 'charizard@rose-hulman.edu', '$2y$10$K3B4eIZIjBs2dOiqJsPAee9MN3l/g3FlsJrKmxIs0pWkE68qwhnJ6','Male');	# generate_tables
+    SET @o = Create_Person('Hannah', 'Smith', 'smithyboi@rose-hulman.edu', '$2y$10$WYbkEbq.B65XO8o65fD9hOq93cB04Z3PsOzJVwj2ON8H00Uz9IZuW','Female');	# bighandsmallhand
+    SET @p = Create_Person('Laura', 'Hulman', 'rosehulman@rose-hulman.edu', '$2y$10$gDg1bDzTcA7iLgbMKt2hse3iIwSEhwpXDDtAbBPXqVqcKxtR23gvK','Female');	# blondebiotch
+    SET @q = Create_Person('Lia', 'Apple', 'google@rose-hulman.edu', '$2y$10$z9EEQ5GfbMwhcq9zWFqz8OY5JC8r.ULMXUnpmBwBOHCPNjo.l4Wb2','Female');	# phpmysqljava
+
+END//
+
+CREATE PROCEDURE `Add_Player_Data`()
+BEGIN
+
+	SET @a = Create_Player('Joe', 'Hood', 'hood.joe@rose-hulman.edu', null, 'Male');
+    SET @b = Create_Player('Johnson', 'Joe', 'johnson.joe@rose-hulman.edu', null, 'Male');
+    SET @c = Create_Player('Jacob', 'Petrisko', 'petrisjj@rose-hulman.edu', null, 'Male');
+    SET @d = Create_Player('Jose', 'Alvarez', 'alvarez@rose-hulman.edu', null, 'Male');
+    SET @e = Create_Player('Bob', 'Sutton', 'sutton@rose-hulman.edu', null, 'Male');
+	SET @f = Create_Player('James', 'Taylor', 'tayjames@rose-hulman.edu', null, 'Male');
+    SET @g = Create_Player('Jack', 'Ryan', 'ryan.jack@rose-hulman.edu', null, 'Male');
+    SET @h = Create_Player('Praneet', 'Chakraborty', 'chakrap@rose-hulman.edu', null, 'Male'); # up until here, already created in Add_Person
+    SET @i = Create_Player('Abby', 'Shang', 'shang@rose-hulman.edu', '$2y$10$EOgK0MSi6G4WTdrZV74EZ.ijS7sN8Hvm0oYGmG2BTijuEoFGzTqqa', 'Female');	# Shang69!
+	SET @j = Create_Player('Dell', 'Laptop', 'laptop@rose-hulman.edu', '$2y$10$SBeaBiom.I.jCokegd8x5eXKTyh79ABl7sm2N698WFbF/aRZkD94q', 'Female');	# female@rose
+	SET @k = Create_Player('Up', 'Down', 'direction@rose-hulman.edu', '$2y$10$Xnr0IumiPdDtHT4hCwf1ZetbSbMGaFROFbdDBpoWk3fbLXyfrsVwC', 'Female');	# female@rose!2
+	SET @l = Create_Player('Mike', 'Jagger', 'jagger@rose-hulman.edu', '$2y$10$quSKDGedI.VxWb.BfBFO6uxTUY0BcguoPBvPpc.7/5oNWkP5hg4J2', 'Male');	# malesgaloreYEA
+	SET @m = Create_Player('Donald', 'Trump', 'thebiggest@rose-hulman.edu', '$2y$10$3TRFkuWDabeAWpWwwhPEK.nKkuR9zhLJBHZTrXCAoLZGqqtYAoJI6', 'Male');	# WeWillBuildAWall
+    
+END//
+
+CREATE PROCEDURE `Add_Referee_Data`()
+BEGIN
+
+	SET @i = Create_Referee('Willy', 'Smith', 'bruh@rose-hulman.edu', null, 'Male');
+    SET @j = Create_Referee('Ryan', 'Dinkleburg', 'dinklebuuuuuurg@rose-hulman.edu', null, 'Male');
+    SET @k = Create_Referee('Sam', 'Flickinger', 'flick@rose-hulman.edu', null, 'Male');
+    SET @l = Create_Referee('Chang', 'Wang', 'wutangclan@rose-hulman.edu', null, 'Male');
+    SET @m = Create_Referee('Reeve', 'Vixon', 'vixon@rose-hulman.edu', null, 'Male');
+    SET @n = Create_Referee('Alexander', 'Pikachu', 'charizard@rose-hulman.edu', null, 'Male');
+    SET @o = Create_Referee('Hannah', 'Smith', 'smithyboi@rose-hulman.edu', null, 'Female');
+    SET @p = Create_Referee('Laura', 'Hulman', 'rosehulman@rose-hulman.edu', null, 'Female');
+    SET @q = Create_Referee('Lia', 'Apple', 'google@rose-hulman.edu', null, 'Female');	# already exists in Add_Person
+    SET @a = Create_Referee('Rob', 'Coons', 'coons@rose-hulman.edu', '$2y$10$IZKGs6hC79oI0Y9SqaVN5.I6SN7esmTZZbPUesnNe.cODkqHiFNPi', 'Male');	# ManchesterUnitedRashford10
+    SET @b = Create_Referee('Sriram', 'Mohan', 'mohan@rose-hulman.edu', '$2y$10$eUQSIsGv8NpIrIuPv2PfTexEKZgUwlbLYF/cDBNVlhBv6igzP3G/q', 'Male');	# roseIMStuff
+    
+END//
+
+CREATE PROCEDURE `Add_Team_Data`()
+BEGIN
+
+	Set @a = Create_Team('Hit Squad', 17);
+    Set @b = Create_Team('Layout Losers', 13);
+    Set @c = Create_Team('Ultimate Team', 13);
+    Set @d = Create_Team('AOII', 8);
+    Set @e = Create_Team('Setting Ducks', 17);
+    Set @f = Create_Team('BSBabes', 9);
+    Set @g = Create_Team('Ball Busters', 9);
+    Set @h = Create_Team('Big Baseball',1);
+    Set @i = Create_Team('Easy Money',1);
+    Set @j = Create_Team('Thisccccc',15);
+    Set @k = Create_Team('Jump Score',15);
+    Set @l = Create_Team('DSig', 8);
+    Set @m = Create_Team('FIJI', 8);
+    Set @n = Create_Team('BSB2', 9);
+
+END//
+
+CREATE PROCEDURE `Add_PlaysOn_Data`(IN input int)
+BEGIN
+	
+    Set @x = -25;
+
+	Set @a = Create_PlaysOn(1, 26+@x, 'Captain');
+	Set @b = Create_PlaysOn(2, 27+@x, 'Captain');
+    Set @c = Create_PlaysOn(3, 28+@x, 'Captain');
+    Set @d = Create_PlaysOn(4, 29+@x, 'Captain');
+    Set @e = Create_PlaysOn(5, 30+@x, 'Captain');
+    Set @f = Create_PlaysOn(6, 31+@x, 'Captain');
+    Set @g = Create_PlaysOn(7, 32+@x, 'Captain');
+    Set @h = Create_PlaysOn(8, 33+@x, 'Captain');
+    Set @i = Create_PlaysOn(18, 34+@x, 'Captain');
+	Set @j = Create_PlaysOn(19, 35+@x, 'Captain');
+    Set @k = Create_PlaysOn(20, 36+@x, 'Captain');
+    Set @l = Create_PlaysOn(21, 37+@x, 'Captain');
+    Set @m = Create_PlaysOn(22, 38+@x, 'Captain');
+    Set @n = Create_PlaysOn(23, 39+@x, 'Captain');
+    Set @o = Create_PlaysOn(24, 26+@x, 'Player');
+    Set @p = Create_PlaysOn(25, 27+@x, 'Player');
+    Set @q = Create_PlaysOn(26, 28+@x, 'Player');
+    Set @r = Create_PlaysOn(27, 29+@x, 'Player');
+    Set @s = Create_PlaysOn(28, 30+@x, 'Player');
+    Set @t = Create_PlaysOn(29, 31+@x, 'Player');
+    Set @u = Create_PlaysOn(30, 32+@x, 'Player');
+    Set @v = Create_PlaysOn(31, 33+@x, 'Player');
+
+END//
+
+CREATE PROCEDURE `Get_Games_Ref`(IN mail varchar(50))
+BEGIN
+   SELECT game_ID, Team1 as Team1_ID, Team2 as Team2_ID, (SELECT location FROM Facility WHERE facility_ID = facility) AS Location, StartDateTime AS StartTime, (SELECT name FROM Team WHERE team_ID = Plays.team1) as Team1, (SELECT name FROM Team  WHERE team_ID = Plays.team2) AS Team2, home_Score as Team1Score, away_Score as Team2Score
+    FROM Game
+    JOIN Plays ON game = game_ID
+    WHERE ref = (SELECT person_ID FROM Person WHERE email = mail);
+END//
+
+CREATE  PROCEDURE `get_permission`(IN mail varchar(50), OUT permission varchar(20))
+BEGIN
+
+SET @id = (SELECT person_ID FROM Person WHERE email = mail);
+
+IF(@id IN (SELECT person_ID FROM Player))
+THEN
+SELECT 'Player' INTO permission;
+END IF;
+
+IF(@id IN (SELECT person_ID FROM Referee))
+THEN
+SELECT 'Referee' INTO permission;
+END IF;
+
+IF(@id IN (SELECT player FROM PlaysOn WHERE role = 'Captain'))
+THEN
+SELECT 'Captain' INTO permission;
+END IF;
+
+END//
+
+CREATE PROCEDURE `Get_Roster`(IN teamID int)
+BEGIN
+
+SELECT Person.firstName as First, Person.lastName as Last, role as Role
+FROM Person
+JOIN PlaysOn ON player = person_ID
+JOIN Team ON team_ID = team
+WHERE team_ID = teamID;
+
+END//
+
+CREATE PROCEDURE `Get_Schedule`(IN teamid int)
+BEGIN
+    
+    Select Plays.team1 as Team1_ID, Plays.team2 as Team2_ID,  (SELECT name FROM Team WHERE team_ID = Plays.team1) as Team1,  (SELECT name FROM Team WHERE team_ID = Plays.team2) as Team2, Game.StartDateTime as StartTime, Facility.location as  Location, home_Score as Team1Score, away_Score as Team2Score
+    From Plays
+		Join Game ON Plays.game = Game.game_ID
+        Join Facility ON Facility.facility_ID = Game.facility
+	Where Plays.team1 = teamid OR Plays.team2 = teamid;
+    
+END//
+
+CREATE PROCEDURE `Get_Teams`(
+IN mail varchar(50))
+BEGIN
+
+	SET @id = (SELECT person_ID FROM Person WHERE email = mail);
+    
+    Select league_ID, team_ID, sport as Sport, League.name as League, Team.name as Team, (SELECT rules FROM Sport WHERE name = sport) as Rules
+    From League
+    Join Team ON League.league_ID = Team.league
+    Where Team.team_ID IN (Select team
+							From PlaysOn
+							Where player = @id);
+
+END//
+
+CREATE PROCEDURE `League_Standings`(IN leagueID INT)
+BEGIN
+SELECT name, wins, losses
+FROM Team_Win_Percentage
+WHERE league_ID = leagueID
+ORDER BY WinPercentage DESC;
+
+END//
+
+CREATE PROCEDURE `Remove_League_Data`()
+BEGIN
+	DELETE FROM League;
+END//
+
+CREATE PROCEDURE `Remove_Sport_Data`()
+BEGIN
+
+	DELETE FROM Sport;
+
+END//
